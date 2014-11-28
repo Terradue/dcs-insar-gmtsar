313d6ce86ec9b9a10478fe6ac5be4db766743b42
Fabrice Brito <fabrice.brito@terradue.com>
2014-11-28 14:27:40 +0100

    Update run.sh

--- src/main/app-resources/gmtsar/run.sh
+++ src/main/app-resources/gmtsar/run.sh
@@ -23,23 +23,22 @@ ERR_NOSLAVEWKT=20
 ERR_NOSLAVEFILE=25
 
 # add a trap to exit gracefully
-function cleanExit ()
-{
-   local retval=$?
-   local msg=""
-   case "$retval" in
-     $SUCCESS)      	msg="Processing successfully concluded";;
-     $ERR_NOMASTER) 	msg="Master reference not provided";;
-     $ERR_NOMASTERWKT) 	msg="Master WKT not retrieved";;
-     $ERR_NOMASTERFILE)	msg="Master not retrieved to local node";;	
-     $ERR_NODEM)    	msg="DEM not retrieved";;
-     $ERR_AUX)          msg="Failed to retrieve auxiliary and/or orbital data";;
-     $ERR_NOCEOS)	msg="CEOS product not retrieved";;
-     $ERR_NOSLAVEFILE) msg="Slave not retrieved to local node";;
-     *)             	msg="Unknown error";;
-   esac
-   [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
-   exit $retval
+function cleanExit () {
+  local retval=$?
+  local msg=""
+  case "$retval" in
+    $SUCCESS)      	    msg="Processing successfully concluded";;
+    $ERR_NOMASTER) 	    msg="Master reference not provided";;
+    $ERR_NOMASTERWKT) 	msg="Master WKT not retrieved";;
+    $ERR_NOMASTERFILE)	msg="Master not retrieved to local node";;	
+    $ERR_NODEM)    	    msg="DEM not retrieved";;
+    $ERR_AUX)           msg="Failed to retrieve auxiliary and/or orbital data";;
+    $ERR_NOCEOS)	      msg="CEOS product not retrieved";;
+    $ERR_NOSLAVEFILE)   msg="Slave not retrieved to local node";;
+    *)             	    msg="Unknown error";;
+  esac
+  [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
+  exit $retval
 }
 trap cleanExit EXIT
 
@@ -49,107 +48,98 @@ mkdir -p $TMPDIR/runtime/raw $TMPDIR/runtime/topo $TMPDIR/runtime/log &> /dev/nu
 mkdir -p $TMPDIR/aux/ENVI/ASA_INS
 mkdir -p $TMPDIR/aux/ENVI/Doris
 
+export ORBITS=$TMPDIR/aux
+
 first="true"
 while read input
 do
-    refs=`ciop-copy -o $TMPDIR $input`
-    # get the DEM for the first time
-    [ "$first" == "true" ] && {
-      ciop-log "INFO" "Retrieve DEM"	 
-      demurl=`cat $refs | egrep -v '(aux=|vor=|slave=|master=)' | cut -d "=" -f 2-`
-      echo $demurl | ciop-copy -o $TMPDIR/runtime/topo -
-      	
-      [ "$?" != "0" ] && exit $ERR_NODEM
-      ciop-log "DEBUG" "`tree $TMPDIR/runtime/topo`"
-      first="false"
-    }
+  refs=`ciop-copy -o $TMPDIR $input`
+  # get the DEM for the first time
+  [ "$first" == "true" ] && {
+    ciop-log "INFO" "Retrieve DEM"	 
+    demurl=`cat $refs | egrep -v '(aux=|vor=|slave=|master=)'`
+    echo ${demurl#dem=} | ciop-copy -o $TMPDIR/runtime/topo -
     
-    # get the aux and orbital data
-    cat $refs | egrep -v '(dem=|slave=|master=)' | while read url
-    do
-      ciop-log "INFO" "Getting $url"
-      ciop-copy -o $TMPDIR/aux `echo $url | cut -d "=" -f 2`
-      [ "$?" != "0" ] && exit $ERR_AUX
-    done 
+    [ "$?" != "0" ] && exit $ERR_NODEM
+    ciop-log "DEBUG" "`tree $TMPDIR/runtime/topo`"
+    first="false"
+  }
     
-    export ORBITS=$TMPDIR/aux
-    cp $TMPDIR/aux/DOR* $TMPDIR/aux/ENVI/Doris
-    cp $TMPDIR/aux/ASA_INS* $TMPDIR/aux/ENVI/ASA_INS
-    cd $TMPDIR/aux/ENVI/ASA_INS    
-    ls ASA_INS* > $TMPDIR/aux/ENVI/ASA_INS/list
-    cd $TMPDIR
+  # get the aux and orbital data
+  cat $refs | egrep -v '(dem=|slave=|master=|aux=)' | while read url
+  do
+    ciop-log "INFO" "Getting $url"
+    echo ${url#dor=} | ciop-copy -o $TMPDIR/aux/ENVI/Doris -`
+    [ "$?" != "0" ] && exit $ERR_AUX
+  done 
+   
+  cat $refs | egrep -v '(dem=|slave=|master=|dor=)' | while read url
+  do
+    ciop-log "INFO" "Getting $url"
+    echo ${url#aux=} | ciop-copy -o $TMPDIR/aux/ENVI/ASA_INS -`
+    [ "$?" != "0" ] && exit $ERR_AUX
+  done
+  # create the list of ASA_INS_AX
+  ls ASA_INS* > $TMPDIR/aux/ENVI/ASA_INS/list
+  cd $TMPDIR
 
-    # get the references to master and slave
-    master=`cat $refs | grep "master=" | cut -d "=" -f 2`
-    slave=`cat $refs | grep "slave=" | cut -d "=" -f 2`
+  # get the references to master and slave
+  master=`cat $refs | egrep -v '(dem=|dor=|aux=|slave=)'`
+  slave=`cat $refs | egrep -v '(dem=|dor=|aux=|master=)'`
 	
-    # Get the master
-    ciop-log "INFO" "Retrieve $master from archive"
-    # from reference to local path
-    master=`echo $master | ciop-copy -o $TMPDIR/runtime/raw -`
+  # Get the master
+  ciop-log "INFO" "Retrieve $master from archive"
+  master=`echo ${master#master=} | ciop-copy -o $TMPDIR/runtime/raw -`
+  [ -z "$master" ] && exit $ERR_NOMASTERFILE
 
-    ciop-log "DEBUG" "master: $master"
-    [ -z "$master" ] && exit $ERR_NOMASTERFILE
+  cd $TMPDIR/runtime/raw
+  [[ $master == *CEOS* ]] && {
+    # ERS2 in CEOS format
+    tar --extract --file=$master -O DAT_01.001 > master.dat
+    tar --extract --file=$master -O LEA_01.001 > master.ldr
+    [ ! -e $TMPDIR/runtime/raw/master.dat ] && exit $ERR_NOCEOS
+    [ ! -e $TMPDIR/runtime/raw/master.ldr ] && exit $ERR_NOCEOS
+   } || {
+    # ENVISAT ASAR in N1 format
+    ln -s $master master.baq
+  }
 
-	 cd $TMPDIR/runtime/raw
-	 [[ $master == *CEOS* ]] && {
-		# ERS2 in CEOS format
-		tar --extract --file=$master -O DAT_01.001 > master.dat
-        	tar --extract --file=$master -O LEA_01.001 > master.ldr
-		[ ! -e $TMPDIR/runtime/raw/master.dat ] && exit $ERR_NOCEOS
-		[ ! -e $TMPDIR/runtime/raw/master.ldr ] && exit $ERR_NOCEOS
-	 } || {
-      # ENVISAT ASAR in N1 format
-      ln -s $master master.baq
-	 }
+  ciop-log "INFO" "Retrieve slave $slave from archive"
+  slave=`echo ${slave#slave=} | ciop-copy -o $TMPDIR/runtime/raw -`
+  [ -z "$slave" ] && exit $ERR_NOSLAVEFILE
 
-	ciop-log "INFO" "Retrieve slave $slave from archive"
-	slave=`echo $slave | ciop-copy -o $TMPDIR/runtime/raw -`
-	[ -z "$slave" ] && exit $ERR_NOSLAVEFILE
+  ciop-log "INFO" "GMTSAR processing for slave `basename $slave`"
+  # ERS2 in CEOS format
+  [[ $slave == *CEOS* ]] && {	
+    tar --extract --file=$slave -O DAT_01.001 > $TMPDIR/runtime/raw/slave.dat
+    tar --extract --file=$slave -O LEA_01.001 > $TMPDIR/runtime/raw/slave.ldr
+    flag="ers"
+  } || { 	
+    # ENVISAT ASAR in N1 format
+    ln -s $slave $TMPDIR/runtime/raw/slave.baq
+    flag="envi"
+  }
 
-	ciop-log "INFO" "GMTSAR processing for slave `basename $slave`"
-	# ERS2 in CEOS format
-	[[ $slave == *CEOS* ]] && {	
-		tar --extract --file=$slave -O DAT_01.001 > $TMPDIR/runtime/raw/slave.dat
-      tar --extract --file=$slave -O LEA_01.001 > $TMPDIR/runtime/raw/slave.ldr
-		result=`echo "${master}_${slave}" | sed 's#.*/\(.*\)\.N1_.*/\(.*\)\.N1#\1_\2#g'`
-		csh $_CIOP_APPLICATION_PATH/gmtsar/libexec/run_ers.csh &> $TMPDIR/runtime/$(result)_ers.log
-		ciop-publish -m $TMPDIR/runtime/$(result)_ers.log
-	} || { 	
-		set -x
-        	# ENVISAT ASAR in N1 format
-		ln -s $slave $TMPDIR/runtime/raw/slave.baq
-		result=`echo "${master}_${slave}" | sed 's#.*/\(.*\)\.N1_.*/\(.*\)\.N1#\1_\2#g'`
-		ciop-log "DEBUG" "`tree $TMPDIR/runtime`"
-                csh $_CIOP_APPLICATION_PATH/gmtsar/libexec/run_envi.csh & #> $TMPDIR/runtime/${result}_envi.log &
-		wait ${!}			
-                ciop-log "DEBUG" "`tree $TMPDIR/runtime`"
-		ciop-log "INFO" "Publishing log"
-		ciop-publish -m $TMPDIR/runtime/${result}_envi.log
-		set +x
-		exit 1	
-       }
+  result=`echo "${master}_${slave}" | sed 's#.*/\(.*\)\.N1_.*/\(.*\)\.N1#\1_\2#g'`
+  csh $_CIOP_APPLICATION_PATH/gmtsar/libexec/run_${flag}.csh & #> $TMPDIR/runtime/${result}_envi.log &
+  wait ${!}			
 
 	# publish results and logs
-	ciop-log "INFO" "result packaging"
+	ciop-publish -m $TMPDIR/runtime/${result}_${flag}.log
 	
+	ciop-log "INFO" "result packaging"
 	cd $TMPDIR/runtime/intf
 	
-	tar vcfzh $result.tgz . 1>&2 #&> /dev/null
+	tar -C $TMPDIR/runtime/intf -fzh $result.tgz . 
 
 	ciop-log "INFO" "publish results and logs"
-	ciop-publish -m $TMPDIR/runtime/intf/$result.tgz
+	ciop-publish -m $TMPDIR/$result.tgz
 	
 	ciop-log "INFO" "cleanup"
 	
 	[ -d "$TMPDIR" ] && {
-		cd $TMPDIR/runtime/raw
-		rm -f *
-
-		cd $TMPDIR/runtime/intf
-		rm -fr *
-
+		rm -fr $TMPDIR/runtime/raw/*
+		rm -fr $TMPDIR/runtime/intf/*
 	}	
 
 done
-
